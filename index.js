2efe7661fa675a76dc6282982f9854c41e1828c5
Jayden Patel <jpatel.sh@mysummitps.org>
2016-08-31 22:22:28 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// JavaScript File
+var aboutdiv = document.getElementById("About-Div");
+var aboutspan = document.getElementById("About-Span");
+aboutdiv.addEventListener("mouseover", function (){
+    aboutspan.innerHTML === "test"
+});
