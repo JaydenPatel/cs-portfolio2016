3186df6348f17b8c67004dbe08f2222bf20a8b46
JaydenPatel <jpatel.sh@mysummitps.org>
2016-09-01 08:30:31 -0700

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// JavaScript File
-var aboutdiv = document.getElementById("About-Div");
-var aboutspan = document.getElementById("About-Span");
-aboutdiv.addEventListener("mouseover", function (){
-    aboutspan.innerHTML === "test"
-});
